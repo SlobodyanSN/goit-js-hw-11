1236d84b087252310adc1273e2c767c34eb49fff
SlobodyanSN <110773219+SlobodyanSN@users.noreply.github.com>
2023-03-13 21:45:39 +0200

    working_markup

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 
 import { fethcImg } from "./axios";
 const input = document.querySelector(`#search-form`);
-const gallery = document.querySelector(`gallery`)
+const gallery = document.querySelector(`.gallery`)
 
  function createMarkup(hits) {
        const listMarkup = 
        hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads
         }) => {
             return `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>${views}</b>
-    </p>
-    <p class="info-item">
-      <b>${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>${downloads}</b>
-    </p>
-  </div>
-</div>` }).join("");
+            <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" width=320 height=240/></a>
+            <div class="info">
+            <p class="info-item">Likes: 
+              <b>${likes}</b>
+            </p>
+              <p class="info-item">Views: 
+                <b>${views}</b>
+              </p>
+              <p class="info-item">Comments: 
+                <b>${comments}</b>
+              </p>
+              <p class="info-item">Downloads:
+                <b>${downloads}</b> 
+              </p>
+            </div>
+          </div>` }).join("");
        console.log(listMarkup);
       gallery.insertAdjacentHTML(`beforeend`,listMarkup); 
       }
