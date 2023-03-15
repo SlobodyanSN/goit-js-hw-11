5250be9b9545c5d6d0af2dbad45d0f8847011497
SlobodyanSN <110773219+SlobodyanSN@users.noreply.github.com>
2023-03-15 23:31:30 +0200

    wisout_cathc

--- src/axios.js
+++ src/axios.js
@@ -3,11 +3,11 @@ import Notiflix from "notiflix";
 const BASE_URL = `https://pixabay.com/api/?key=`;
 const KEY = `34229211-437131f2e92cbbf7829eea8a9`
 const limit_per_page = 40;
-let page = 2;
 
 
 
-export async function fethcImg(serhcImg) {
+
+export async function fethcImg(serhcImg, page = 1) {
     try{
 const response = await axios(`${BASE_URL}${KEY}`,{params:{
       
@@ -20,18 +20,20 @@ const response = await axios(`${BASE_URL}${KEY}`,{params:{
 }
 })
 if (response.status !== 200) {
-    Notiflix.Notify.failure(`${response.statusText}`)
-    return
+    throw new Error(
+    Notiflix.Notify.failure(`${response.statusText}`))
+    
 }
 if (response.data.total === 0 ) {
-    Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-    return
+    throw new Error(
+    Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again."))
+  
 }
 
 Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images.`)
-// console.log(response);
-const hits = await response.data.hits
-return hits
+console.log(response);
+const data = await response.data
+return data
 }
 catch(error) {
     console.log(error);
--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const input = document.querySelector(`#search-form`);
-const gallery = document.querySelector(`.gallery`)
+const gallery = document.querySelector(`.gallery`);
+const load_more = document.querySelector(`.load-more`);
+load_more.hidden = true;
+let page = 1;
 
 const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 500,
   captionsData: 'alt'});
@@ -14,9 +17,9 @@ function clearGalery() {
   gallery.innerHTML = ``;
 }
 
- function createMarkup(hits) {
+ function createMarkup(data) {
        const listMarkup = 
-       hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads
+       data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads
         }) => {
             return `<div class="photo-card">
             <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" width=300 height=200/></a>
@@ -37,6 +40,9 @@ function clearGalery() {
           </div>` }).join("");
      
       gallery.insertAdjacentHTML(`beforeend`,listMarkup); 
+     
+      load_more.hidden = false;
+      return data
       }
 
 input.addEventListener(`submit`, onSubmit);
@@ -45,10 +51,22 @@ function onSubmit(event) {
 event.preventDefault();
 const serhcImg = event.target.elements.searchQuery.value.trim();
 if (serhcImg ==="") {
-  Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-  return
-}
-fethcImg(serhcImg)
-.then(hits => {createMarkup(hits)})
+  throw new Error(
+  Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again."))
 
-};
+}
+fethcImg(serhcImg, page)
+.then(data => {createMarkup(data)})
+.catch(error)
+
+load_more.addEventListener(`click`, addPage);
+function addPage() {
+page +=1
+fethcImg(serhcImg, page)
+.then(data => {createMarkup(data)})
+.catch(error) 
+  
+}
+
+}
+
